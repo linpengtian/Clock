74795f50f904ed2402873cb9c2d2604a150eaead
Lin PengTian <69813618+linpengtian@users.noreply.github.com>
2020-08-20 12:26:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+    d = new Date();
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    var offset = timezone.value;
+    console.log(offset);
+    var nd = new Date(utc + (3600000 * offset));
+    htime = nd.getHours();
+    mtime = nd.getMinutes();
+    stime = nd.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
